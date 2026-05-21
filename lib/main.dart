4a2977c763bdb6973875bf1215d27f0ed0846266
tDui <142105319+tDiui@users.noreply.github.com>
2026-05-21 10:34:02 +0700

    Update Supabase initialization comments
    Removed sensitive information from Supabase initialization.

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,8 @@ Future<void> main() async {
 
   // ⚡ Dán chính xác 2 chuỗi bạn vừa lấy vào đây:
   await Supabase.initialize(
-    url: 'https://twswetznypblkttdusic.supabase.co', // 🔗 Đường link lấy từ mục General Settings
-    anonKey: 'sb_publishable_JqpParGh-m3hubkRCEqRmA_jmY9YTnI', // 🔑 Chuỗi lấy từ mục Publishable key
+     // 🔗 Đường link lấy từ mục General Settings
+     // 🔑 Chuỗi lấy từ mục Publishable key
   );
 
   runApp(const LaptopAIApp());
